09a818ea91a9c192c76fa42d7f72a3f0034d33bc
siddish reddy <Siddish.ibv@gmail.com>
2018-01-20 07:36:15 +0530

    Update Primality.py

--- Primality.py
+++ Primality.py
@@ -1,8 +1,23 @@
 def isPrime(n):
-    for i in range(2,n//2):
-        if (n%i==0):
-            return False;
+    if (n <= 1)  return False;
+    if (n <= 3)  return True;
+    if (n%2 == 0 or n%3 == 0) return False;
+    """ for i in range(2, n//2):
+            if (n%i==0):
+                return False; 
+    is normal version we learnt """
+    
+    i=5
+    w=2
+    while i*i<= n:
+        if n % i == 0:
+            return False
+        i += w
+        w = 6 - w
     return True
+"""It's a variant of the classic O(sqrt(N)) algorithm. 
+It uses the fact that a prime (except 2 and 3) is of form 6k - 1 or 6k + 1 and looks only at divisors of this form
+https://stackoverflow.com/questions/1801391/what-is-the-best-algorithm-for-checking-if-a-number-is-prime"""
 
 n = int(input("Enter the number to check whether it is prime or not: "))
 if isPrime(n):
