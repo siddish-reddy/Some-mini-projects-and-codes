283ec0c6245e57e4d9f1e10aa109f8b8cc6ddbaa
siddish reddy <Siddish.ibv@gmail.com>
2018-01-20 07:40:30 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,4 +1,4 @@
-def fact(n):
+def fact(n):    #this is basic version of finding factorial of a number go to https://www.geeksforgeeks.org/factorial-large-number/ for large numbers
     if n == 0:
         return 1
     else :
